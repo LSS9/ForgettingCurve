eb86ce1107d5dfbf2edef9cde0d3ea648a259290
lss <1027224328@qq.com>
2023-06-09 18:41:06 +0800

    规范代码格式，针对长时间未学习的任务，优化其下一次提醒时间

--- src/main/java/com/project/base/BaseController.java
+++ src/main/java/com/project/base/BaseController.java
@@ -1,16 +1,9 @@
-package com.project.configurer;
+package com.project.base;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @Controller
 @RequestMapping("/")
